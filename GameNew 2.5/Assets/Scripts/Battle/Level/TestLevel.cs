4b0d1a4c746410664dce5dc15faa7fb55f62967a
Fatmir Gusani <G00338685@gmit.ie>
2020-02-09 20:09:27 +0000

    Experiece and Level Fully Working.
    The Experience system and Level system working fully.

---
+++ GameNew 2.5/Assets/Scripts/Battle/Level/TestLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLevel : MonoBehaviour
+{
+
+    //[SerializeField] private DisplayLevel displayLevel;
+    
+    public DisplayLevel displayLevel;
+    public LevelSystem levelSystem;
+    public MyHealthSystem myHealthSystem;
+
+    private void Awake()
+    {
+        LevelSystem levelSystem = new LevelSystem();
+        //myHealthSystem = new MyHealthSystem(37);
+        displayLevel.SetLevelSystem(levelSystem);
+    }
+}
